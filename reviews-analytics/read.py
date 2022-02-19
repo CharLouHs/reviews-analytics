3f195f0941c8b3b595345ce88c24d6c04290e818
CharLouHs <chacptzy258@gmail.com>
2022-02-19 17:12:41 +0800

    add filter

--- reviews-analytics/read.py
+++ reviews-analytics/read.py
@@ -12,3 +12,9 @@ print('檔案讀取完，共有',len(data),'筆資料')
 avg= k/len(data)
 print('平均長度為: ',avg ,' 個字')
 print(data[0])
+
+new=[]
+for d in data:
+    if len(d)<100:
+        new.append(d)
+print('一共有',len(new),'筆留言長度小於100')
